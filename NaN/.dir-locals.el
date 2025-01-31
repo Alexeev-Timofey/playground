ae582f74a616d2b262eeae4193d0b29926c5bd7f
Timofey Alekseev <alekseev-t@protei.ru>
2025-01-31 18:40:40 +0300

    sync

--- NaN/.dir-locals.el
+++ NaN/.dir-locals.el
@@ -1,1 +1,3 @@
-((auto-mode-alist . (("SConstruct$" . python-mode))))
+((nil . ((eglot-server-programs . ((python-mode . ("/home/alekseevt/Playground/playground/.pipenv/.venv/bin/pylsp"))))))
+ (python-mode . ((eval . (python-default-setup-hook))))
+ (auto-mode-alist . (("SConstruct$" . python-mode))))
